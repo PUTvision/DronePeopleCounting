b9ca8db67dba20120396255070106267bf2022e1
Bartosz Ptak <bartptak@gmail.com>
2022-05-04 07:45:14 +0000

    Fix metric tests

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -3,14 +3,13 @@ import pytest
 import torch
 
 from src.metrics.counting_mae_metric import CountingMAEMetric
-from src.metrics.dice_metric import DiceMetric
 
 
 def test_PeopleMAE():
     metric = CountingMAEMetric()
     zeros = torch.zeros((4, 1, 32, 32))
     dest = torch.zeros((4, 1, 32, 32))
-    dest[:, 0, 1, :5] = 100
+    dest[:, 0, 1, :5] = 400
     metric.update(dest, zeros)
     assert torch.isclose(metric.compute(), torch.tensor(5.0))
     del metric
@@ -18,11 +17,11 @@ def test_PeopleMAE():
     metric = CountingMAEMetric()
     zeros = torch.zeros((4, 1, 32, 32))
     dest = torch.zeros((4, 1, 32, 32))
-    dest[0, 0, 1, :2] = 100
+    dest[0, 0, 1, :2] = 400
     metric.update(dest, zeros)
     assert torch.isclose(metric.compute(), torch.tensor(0.5))
 
-    dest[1, 0, 1, 10:] = 100
+    dest[1, 0, 1, 10:] = 400
     metric.update(dest, zeros)
     assert torch.isclose(metric.compute(), torch.tensor(3.25))
 
